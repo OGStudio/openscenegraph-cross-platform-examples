7a345ede9c0519c6d9385d9e57f3ce7bba4211e3
Michael Kapelko <kornerr@gmail.com>
2018-04-23 12:28:30 +0300

    Print Method, URL

--- features/src/app.js
+++ features/src/app.js
@@ -3,8 +3,11 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 7999;
 
-const server =
-    http.createServer((request, response) => {
+const server = http.createServer()
+server.on("request", (request, response) => {
+        const { method, url } = request;
+        console.log(`Got request. Method: ${method}. URL: ${url}`);
+
         response.statusCode = 200;
         response.setHeader("Content-Type", "text/plain");
         response.end("Hello, World\n");
