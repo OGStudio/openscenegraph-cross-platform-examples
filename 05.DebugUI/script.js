b24e65a252d595f03a1f21e5914ff956511bfc34
Michael Kapelko <kornerr@gmail.com>
2018-05-17 19:12:40 +0300

    Add stub index.html

---
+++ 05.DebugUI/script.js
@@ -0,0 +1,5 @@
+// This function is called after page load.
+$(function() {
+    $("div.navigation").html("Here will be pages to select from");
+    $("div.contents").html("Here will be selected page items to alter");
+});
