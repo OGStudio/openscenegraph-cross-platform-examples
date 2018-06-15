1e3868ba86fe672503763e837e50b2b971aaa2dd
Michael Kapelko <kornerr@gmail.com>
2018-06-15 11:56:10 +0300

    Move DebugBroker/DebugUI to their own repos

--- 03.RemoteDebugging/DebugUI/parsing.js
+++
@@ -1,9 +0,0 @@
-
-function itemJSON(itemLocation, itemValue)
-{
-    const itemJSON = `{"title":"${itemLocation.itemTitle}","value":"${itemValue}"}`;
-    const pageJSON = `{"title":"${itemLocation.pageTitle}","items":[${itemJSON}]}`;
-    const debuggerJSON = `{"title":"${itemLocation.debuggerTitle}","pages":[${pageJSON}]}`;
-    return debuggerJSON;
-}
-
