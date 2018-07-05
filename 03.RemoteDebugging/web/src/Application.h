27347281fec77fadb58cf48c9e0b8d393bc057d5
Michael Kapelko <kornerr@gmail.com>
2018-07-05 12:04:12 +0300

    Fix mouse input for Ex03 web

--- 03.RemoteDebugging/web/src/Application.h
+++ 03.RemoteDebugging/web/src/Application.h
@@ -56,6 +56,10 @@ freely, subject to the following restrictions:
 #include "Reporter.h"
 
 // Application+frame+Reporting End
+// Application+handleEvent-web Start
+#include <SDL2/SDL.h>
+
+// Application+handleEvent-web End
 
 // Application+OSGCPE_APPLICATION_LOG Start
 #include "log.h"
@@ -132,6 +136,87 @@ class Application
                 this->frameReporter.report();
             }
         // Application+frame+Reporting End
+        // Application+handleEvent-web Start
+        private:
+            bool fingerEventsDetected = false;
+        public:
+            bool handleEvent(const SDL_Event &e)
+            {
+                // Get event queue.
+                osgViewer::GraphicsWindow *gw =
+                    dynamic_cast<osgViewer::GraphicsWindow *>(
+                        this->viewer->getCamera()->getGraphicsContext());
+                if (!gw)
+                {
+                    return false;
+                }
+                osgGA::EventQueue &queue = *(gw->getEventQueue());
+        
+                // Detect finger events.
+                if (
+                    e.type == SDL_FINGERMOTION ||
+                    e.type == SDL_FINGERDOWN ||
+                    e.type == SDL_FINGERUP
+                ) {
+                    fingerEventsDetected = true;
+                }
+                // Handle mouse events unless finger events are detected.
+                if (!fingerEventsDetected)
+                {
+                    return handleMouseEvent(e, queue);
+                }
+                // Handle finger events.
+                return handleFingerEvent(e, queue);
+            }
+        
+        private:
+            bool handleFingerEvent(const SDL_Event &e, osgGA::EventQueue &queue)
+            {
+                int absX = this->windowWidth * e.tfinger.x;
+                int absY = this->windowHeight * e.tfinger.y;
+                auto correctedY = -(this->windowHeight - absY);
+                switch (e.type)
+                {
+                    case SDL_FINGERMOTION:
+                        queue.mouseMotion(absX, correctedY);
+                        return true;
+                    case SDL_FINGERDOWN: 
+                        queue.mouseButtonPress(absX, correctedY, e.tfinger.fingerId);
+                        return true;
+                    case SDL_FINGERUP:
+                        queue.mouseButtonRelease(absX, correctedY, e.tfinger.fingerId);
+                        return true;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        
+            bool handleMouseEvent(const SDL_Event &e, osgGA::EventQueue &queue)
+            {
+                switch (e.type)
+                {
+                    case SDL_MOUSEMOTION: {
+                        auto correctedY = -(this->windowHeight - e.motion.y);
+                        queue.mouseMotion(e.motion.x, correctedY);
+                        return true;
+                    }
+                    case SDL_MOUSEBUTTONDOWN: {
+                        auto correctedY = -(this->windowHeight - e.button.y);
+                        queue.mouseButtonPress(e.button.x, correctedY, e.button.button);
+                        return true;
+                    }
+                    case SDL_MOUSEBUTTONUP: {
+                        auto correctedY = -(this->windowHeight - e.button.y);
+                        queue.mouseButtonRelease(e.button.x, correctedY, e.button.button);
+                        return true;
+                    }
+                    default:
+                        break;
+                }
+                return false;
+            }
+        // Application+handleEvent-web End
         // Application+setupWindow-embedded Start
         private:
             int windowWidth;
