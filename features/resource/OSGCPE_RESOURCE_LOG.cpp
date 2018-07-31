560368a30aa5f36b14904507ea0ea32495d6de64
Michael Kapelko <kornerr@gmail.com>
2018-07-31 22:41:48 +0300

    Update OSGCPE_RESOURCE_LOG to exclude osgcpe namespace

--- features/resource/OSGCPE_RESOURCE_LOG.cpp
+++ features/resource/OSGCPE_RESOURCE_LOG.cpp
@@ -1,10 +1,10 @@
 FEATURE resource.h/Impl
 #include "log.h"
 #include "format.h"
-#define OSGCPE_RESOURCE_LOG_PREFIX "osgcpe::resource %s"
+#define OSGCPE_RESOURCE_LOG_PREFIX "resource %s"
 #define OSGCPE_RESOURCE_LOG(...) \
-    osgcpe::log::logprintf( \
+    log::logprintf( \
         OSGCPE_RESOURCE_LOG_PREFIX, \
-        osgcpe::format::printfString(__VA_ARGS__).c_str() \
+        format::printfString(__VA_ARGS__).c_str() \
     )
 
