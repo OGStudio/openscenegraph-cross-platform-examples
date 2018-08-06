39e324c152e9978b53e9e9a1046341d309e4f90c
Michael Kapelko <kornerr@gmail.com>
2018-08-06 11:29:39 +0300

    Build Ex01 for macOS

---
+++ libs/OpenSceneGraph/src/osgViewer-macos.mm
@@ -0,0 +1,8 @@
+
+#define USE_DARWIN_COCOA_IMPLEMENTATION
+
+#include "src/osgViewer/DarwinUtils.mm"
+#include "src/osgViewer/GraphicsWindowCocoa.mm"
+#include "src/osgViewer/PixelBufferCocoa.mm"
+
+#include "osgViewer.cpp"
