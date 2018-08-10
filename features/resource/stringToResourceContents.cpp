ac2adc4cb1d8558316d8d963fd90c2b2f9c7d074
Michael Kapelko <kornerr@gmail.com>
2018-08-10 22:31:41 +0300

    Introduce stringToResourceContents

---
+++ features/resource/stringToResourceContents.cpp
@@ -0,0 +1,24 @@
+FEATURE resource.h/Impl
+unsigned char * stringToResourceContents(const std::string &str)
+{
+    auto dat = const_cast<char *>(str.data());
+    if (!dat)
+    {
+        OSGCPE_RESOURCE_LOG(
+            "ERROR Could not convert string to resource contents "
+            "at 'const char * -> char *' stage"
+        );
+        return 0;
+    }
+    auto contents = reinterpret_cast<unsigned char *>(dat);
+    if (!contents)
+    {
+        OSGCPE_RESOURCE_LOG(
+            "ERROR Could not convert string to resource contents "
+            "at 'char * -> usigned char *' stage"
+        );
+        return 0;
+    }
+
+    return contents;
+}
