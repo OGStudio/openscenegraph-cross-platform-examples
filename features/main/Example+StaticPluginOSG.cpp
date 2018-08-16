4417b27c2bf43624fe9eb3e5cce31569da998e32
Michael Kapelko <kornerr@gmail.com>
2018-08-16 11:41:36 +0300

    Include OSG_USEPLUGIN* macro headers

--- features/main/Example+StaticPluginOSG.cpp
+++ features/main/Example+StaticPluginOSG.cpp
@@ -1,4 +1,6 @@
 FEATURE main.h/Impl
+#include <osgDB/Registry>
+
 // Reference (statically) plugins to read `osgt` file.
 USE_OSGPLUGIN(osg2)
 USE_SERIALIZER_WRAPPER_LIBRARY(osg)
--- features/main/Example+StaticPluginPNG.cpp
+++ features/main/Example+StaticPluginPNG.cpp
@@ -1,7 +1,8 @@
 FEATURE main.h/Impl
+#include <osgDB/Registry>
+
 // Reference (statically) plugins to read `png` file.
 // Apple platforms use ImageIO. All others use libpng.
-
 #ifdef __APPLE__
     USE_OSGPLUGIN(imageio)
 #else
