4acd8a8343f2f6c6a4a0893e79e5cd3c74863d82
Michael Kapelko <kornerr@gmail.com>
2018-09-19 12:43:48 +0300

    Update 03

--- 03.HTTPClient/android/app/src/main/cpp/library.cpp
+++ 03.HTTPClient/android/app/src/main/cpp/library.cpp
@@ -32,6 +32,13 @@ freely, subject to the following restrictions:
 
 // library+httpClient-android End
 
+// library+Ex03+JNI-android Start
+#define JNI_FUNC(FUNC_NAME) \
+    JNIEXPORT void JNICALL Java_org_opengamestudio_ex03_library_ ## FUNC_NAME
+#define JNI_ARRAY(FUNC_NAME) \
+    JNIEXPORT jobjectArray JNICALL Java_org_opengamestudio_ex03_library_ ## FUNC_NAME
+#define JNI_ARG JNIEnv *env, jobject /* this */
+// library+Ex03+JNI-android End
 
 using namespace osgcpe;
 
