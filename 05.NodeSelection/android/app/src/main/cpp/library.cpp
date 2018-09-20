ca916f34bb5198a9db35dcf1ff8b8405f480dbc8
Michael Kapelko <kornerr@gmail.com>
2018-09-20 12:03:05 +0300

    Update 05

--- 05.NodeSelection/android/app/src/main/cpp/library.cpp
+++ 05.NodeSelection/android/app/src/main/cpp/library.cpp
@@ -28,6 +28,11 @@ freely, subject to the following restrictions:
 
 // library-android End
 
+// library+Ex05+JNI-android Start
+#define JNI_FUNC(FUNC_NAME) \
+    JNIEXPORT void JNICALL Java_org_opengamestudio_ex05_library_ ## FUNC_NAME
+#define JNI_ARG JNIEnv *env, jobject /* this */
+// library+Ex05+JNI-android End
 
 using namespace osgcpe;
 
